f0a0d9ca264cd4897167d786f079622e4f8ca7a1
saranp2005 <selvansaran10@gmail.com>
2026-07-31 09:49:15 +0530

    Add files via upload

---
+++ salary_descending.sql
@@ -0,0 +1,21 @@
+CREATE TABLE Employee (
+    EmpID INT PRIMARY KEY,
+    Name VARCHAR(50),
+    Department VARCHAR(50),
+    Salary INT,
+    Age INT,
+    City VARCHAR(50)
+);
+INSERT INTO Employee (EmpID, Name, Department, Salary, Age, City) VALUES
+(101, 'Rahul', 'IT', 60000, 25, 'Chennai'),
+(102, 'Priya', 'HR', 45000, 28, 'Coimbatore'),
+(103, 'Arun', 'IT', 70000, 30, 'Madurai'),
+(104, 'Sneha', 'Finance', 55000, 27, 'Chennai'),
+(105, 'Karthik', 'IT', 80000, 35, 'Salem'),
+(106, 'Divya', 'HR', 50000, 24, 'Erode'),
+(107, 'Vijay', 'Finance', 65000, 32, 'Trichy'),
+(108, 'Anitha', 'Marketing', 48000, 26, 'Chennai');
+SELECT *
+FROM Employee
+ORDER BY Salary DESC
+LIMIT 3;
